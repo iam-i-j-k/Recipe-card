f2323589b5bafef237e7a2ec59c98ea1f7894bbf
Irfan <irfanjankhan7860@gmail.com>
2024-10-04 23:12:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById('ingredients-btn').addEventListener('click', function() {
+    const ingredients = document.getElementById('cake-ingredients');
+    ingredients.classList.toggle('hidden');
+});
+
+document.getElementById('instructions-btn').addEventListener('click', function() {
+    const steps = document.getElementById('cake-instructions');
+    steps.classList.toggle('active');
+});
+
+let stepIndex = 0;
+document.getElementById('cook-btn').addEventListener('click', function() {
+    const insts = document.getElementById('cake-instructions');
+    insts.classList.add('active');
+    const steps = document.querySelectorAll('#cake-instructions li');
+    if (stepIndex < steps.length) {
+        document.getElementById('cook').innerHTML = "Next";
+        steps[stepIndex].style.backgroundColor = "#FFCD2A";
+        stepIndex++;
+    }
+    else{
+        document.getElementById('cook-btn').innerHTML = "You have done Cooking";
+
+    }
+});
